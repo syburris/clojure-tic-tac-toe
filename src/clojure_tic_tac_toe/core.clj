f0558e7be6e0088c239ceff8d67f167729894ee0
Steven Burris <stevenburris.jr@gmail.com>
2016-11-02 11:37:03 -0400

    Checked for win

--- src/clojure_tic_tac_toe/core.clj
+++ src/clojure_tic_tac_toe/core.clj
@@ -1,6 +1,32 @@
 (ns clojure-tic-tac-toe.core
   (:gen-class))
 
+(def board [[:x :e :o]
+            [:x :o :e]
+            [:x :e :o]])
+
+
+
 (defn -main [& args]
-  (println "Hello, world!"))
+  (let [[[a b c]
+         [d e f]
+         [g h i]] board
+        solutions (hash-set
+                    [a b c]
+                    [d e f]
+                    [g h i]
+                    [a d g]
+                    [b e h]
+                    [c f i]
+                    [a e i]
+                    [c e g])]
+    (cond
+      (contains? solutions [:x :x :x])
+      (println "X has won!")
+      (contains? solutions [:o :o :o])
+      (println "O has won!")
+      :else
+      (println "No one has won yet."))))
+      
+    
 
